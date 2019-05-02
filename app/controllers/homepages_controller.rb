5edc539534394913cc66eb42ea82645277579d3f
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-02 15:12:44 -0500

    commit 2

--- app/controllers/homepages_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomepagesController < ApplicationController
-  def index
-    @users = User.all
-    @dragons = Dragon.all
-    @set_on_homepage = true
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :dragons
   resources :users
-  get '/' => 'homepages#index', as: :home
+  get '/' => 'users#index', as: :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
